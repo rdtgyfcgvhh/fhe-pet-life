35e9ff9376343fbab73c5cba5584a72f99d076c1
rdtgyfcgvhh <DevensMedler@gmail.com>
2025-11-08 11:35:22 +0800

    Add documentation to home page component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,9 @@
 import { PetLifeDemo } from "@/components/PetLifeDemo";
 
+/**
+ * Home page component
+ * Renders the main PetLife demo interface
+ */
 export default function Home() {
   return (
     <div className="w-full">
